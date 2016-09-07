ae19c2be73e1a6e2deb4570c9e91a961656bafe7
Angelos Karagkiozidis <angelkar@gmail.com>
2016-09-07 15:14:01 +0300

    fixup! fixup! fixup! fixup! Overwrite minitest diff command

--- lib/minitest/assertions.rb
+++ lib/minitest/assertions.rb
@@ -1,53 +1,49 @@
-module Minitest
-  module Assertions
-    ##
-    # Returns a diff between +exp+ and +act+. If there is no known
-    # diff command or if it doesn't make sense to diff the output
-    # (single line, short output), then it simply returns a basic
-    # comparison between the two.
+Minitest::Assertions.module_eval do
+  ##
+  # Returns a diff between +exp+ and +act+. If there is no known
+  # diff command or if it doesn't make sense to diff the output
+  # (single line, short output), then it simply returns a basic
+  # comparison between the two.
 
-    def diff exp, act
-      expect = mu_pp_for_diff exp
-      butwas = mu_pp_for_diff act
-      result = nil
+  def diff exp, act
+    expect = mu_pp_for_diff exp
+    butwas = mu_pp_for_diff act
+    result = nil
 
-      need_to_diff =
-        (expect.include?("\n")    ||
-         butwas.include?("\n")    ||
-         expect.size > 30         ||
-         butwas.size > 30         ||
-         expect == butwas)        &&
+    need_to_diff =
+      (expect.include?("\n") ||
+        butwas.include?("\n") ||
+        expect.size > 30 ||
+        butwas.size > 30 ||
+        expect == butwas) &&
         Minitest::Assertions.diff
 
-      return "Expected: #{mu_pp exp}\n  Actual: #{mu_pp act}" unless
-        need_to_diff
+    return "Expected: #{mu_pp exp}\n  Actual: #{mu_pp act}" unless need_to_diff
 
-      Tempfile.open("expect") do |a|
-        a.puts expect
-        a.flush
+    Tempfile.open("expect") do |a|
+      a.puts expect
+      a.flush
 
-        Tempfile.open("butwas") do |b|
-          b.puts butwas
-          b.flush
+      Tempfile.open("butwas") do |b|
+        b.puts butwas
+        b.flush
 
-          result = `#{Minitest::Assertions.diff} #{a.path} #{b.path} | colordiff`
-          result.sub!(/^\-\-\- .+/, "--- expected")
-          result.sub!(/^\+\+\+ .+/, "+++ actual")
+        result = `#{Minitest::Assertions.diff} #{a.path} #{b.path} | colordiff`
+        result.sub!(/^\-\-\- .+/, "--- expected")
+        result.sub!(/^\+\+\+ .+/, "+++ actual")
 
-          if result.empty? then
-            klass = exp.class
-            result = [
-                      "No visible difference in the #{klass}#inspect output.\n",
-                      "You should look at the implementation of #== on ",
-                      "#{klass} or its members.\n",
-                      expect,
-                     ].join
-          end
+        if result.empty? then
+          klass  = exp.class
+          result = [
+            "No visible difference in the #{klass}#inspect output.\n",
+            "You should look at the implementation of #== on ",
+            "#{klass} or its members.\n",
+            expect,
+          ].join
         end
       end
-
-      result
     end
 
+    result
   end
-end
+
